cdf079d160eb72f0771d79d7cd28a5b526bc6683
Yannic Frank <>
2024-01-26 22:21:24 +0100

    FMOD & Simple Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    public Rigidbody2D physics;
+    public InputAction playerControls;
+
+    private Vector2 moveDirection = Vector2.zero;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveDirection = value.Get<Vector2>();
+    }
+
+    void Move()
+    {
+        physics.velocity = moveDirection * movementSpeed;
+    }
+}

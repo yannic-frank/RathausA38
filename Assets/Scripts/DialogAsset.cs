0ed72d874113bb3858d453abd1906491c4d31da4
Yannic Frank <>
2024-01-27 13:38:57 +0100

    Better Dialog System

--- Assets/Scripts/DialogAsset.cs
+++ Assets/Scripts/DialogAsset.cs
@@ -1,10 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct DialogOption
+{
+    public string text;
+    public List<DialogSequenceEntry> sequence;
+}
+
+[System.Serializable]
+public struct DialogEntry
+{
+    public string text;
+    public List<DialogOption> dialogOptions;
+}
+
+[System.Serializable]
+public struct ChangeFlag
+{
+    public FlagContainer flagContainer;
+    public string flag;
+    public bool enable;
+    public Optional<int> overridePriority;
+}
+
+[System.Serializable]
+public class DialogSequenceEntry
+{
+    public Optional<DialogEntry> dialogEntry;
+    public Optional<DialogAsset> dialogAsset;
+    public Optional<ChangeFlag> changeFlag;
+}
+
 [CreateAssetMenu(fileName = "Dialog", menuName = "ScriptableObjects/DialogAsset", order = 1)]
 public class DialogAsset : ScriptableObject
 {
-    public string text;
-    public List<DialogAsset> nextDialog;
+    public List<DialogSequenceEntry> sequence;
 }
---
+++ Assets/Scripts/Util/Optional.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct Optional<T>
+{
+    [SerializeField] private bool enabled;
+    [SerializeField] private T value;
+
+    public bool Enabled => enabled;
+    public T Value => value;
+
+    public Optional(T initialValue)
+    {
+        enabled = true;
+        value = initialValue;
+    }
+}
+
+namespace Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+    
+    [CustomPropertyDrawer(typeof(Optional<>))]
+    public class OptionalPropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative("value");
+            return EditorGUI.GetPropertyHeight(valueProperty);
+        }
+
+        public override void OnGUI(
+            Rect position,
+            SerializedProperty property,
+            GUIContent label
+        )
+        {
+            var valueProperty = property.FindPropertyRelative("value");
+            var enabledProperty = property.FindPropertyRelative("enabled");
+
+            EditorGUI.BeginProperty(position, label, property);
+            position.width -= 24;
+            EditorGUI.BeginDisabledGroup(!enabledProperty.boolValue);
+            EditorGUI.PropertyField(position, valueProperty, label, true);
+            EditorGUI.EndDisabledGroup();
+
+            int indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+            position.x += position.width + 24;
+            position.width = position.height = EditorGUI.GetPropertyHeight(enabledProperty);
+            position.x -= position.width;
+            EditorGUI.PropertyField(position, enabledProperty, GUIContent.none);
+            EditorGUI.indentLevel = indent;
+            EditorGUI.EndProperty();
+        }
+    }
+}
